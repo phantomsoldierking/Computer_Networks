aa31e45121a597d894e4430ff5aa9e1fe7902c40
phantomsoldierking <patilyogeshgouda@gmail.com>
2025-11-12 11:50:00 +0530

    feat: bi direction multiple devices, pull changes for lab code

--- bi-directional/client.cpp
+++ bi-directional/client.cpp
@@ -15,34 +15,56 @@ void receiveMessages(int socket) {
             std::cout << "Server disconnected.\n";
             break;
         }
-        std::cout << "Server: " << buffer << std::endl;
+        std::cout << "\nServer: " << buffer << std::endl;
+        std::cout << "Client(you): " << std::flush;
     }
 }
 
 void sendMessages(int socket) {
     std::string msg;
     while (true) {
+        std::cout << "Client(You): ";
         std::getline(std::cin, msg);
+        if (msg == "exit") break;
         send(socket, msg.c_str(), msg.size(), 0);
     }
+    close(socket);
+    exit(0);
 }
 
 int main() {
     int sock = 0;
     struct sockaddr_in serv_addr;
 
+    std::string server_ip;
+    std::cout << "Enter server IP address to connect: ";
+    std::cin >> server_ip;
+    std::cin.ignore();
+
     // 1. Create socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
-    // 2. Connect
-    connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    std::cout << "Connected to server!\n";
+    if (inet_pton(AF_INET, server_ip.c_str(), &serv_addr.sin_addr) <= 0) {
+        std::cerr << "Invalid address/Address not supported.\n";
+        return 1;
+    }
 
-    // 3. Launch threads
+    // 2. Connect to server
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        return 1;
+    }
+
+    std::cout << "Connected to server at " << server_ip << ":" << PORT << "!\n";
+
+    // 3. Threads
     std::thread recvThread(receiveMessages, sock);
     std::thread sendThread(sendMessages, sock);
 
--- bi-directional/server.cpp
+++ bi-directional/server.cpp
@@ -15,16 +15,21 @@ void receiveMessages(int socket) {
             std::cout << "Client disconnected.\n";
             break;
         }
-        std::cout << "Client: " << buffer << std::endl;
+        std::cout << "\nClient: " << buffer << std::endl;
+        std::cout << "Server(You): " << std::flush;
     }
 }
 
 void sendMessages(int socket) {
     std::string msg;
     while (true) {
+        std::cout << "Server(You): ";
         std::getline(std::cin, msg);
+        if (msg == "exit") break;
         send(socket, msg.c_str(), msg.size(), 0);
     }
+    close(socket);
+    exit(0);
 }
 
 int main() {
@@ -32,25 +37,46 @@ int main() {
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
+    std::string ip;
+    std::cout << "Enter server IP to bind (usually your local IP, e.g. 192.168.x.x): ";
+    std::cin >> ip;
+    std::cin.ignore();  // clear newline from input buffer
+
     // 1. Create socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("Socket failed");
+        return 1;
+    }
+
+    // Allow reusing the port quickly
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
     // 2. Bind
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
-    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    inet_pton(AF_INET, ip.c_str(), &address.sin_addr);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        return 1;
+    }
 
     // 3. Listen
     listen(server_fd, 3);
-    std::cout << "Server listening on port " << PORT << "...\n";
+    std::cout << "Server listening on " << ip << ":" << PORT << "...\n";
 
-    // 4. Accept client
-    new_socket = accept(server_fd, (struct sockaddr*)&address,
-                        (socklen_t*)&addrlen);
+    // 4. Accept
+    new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+    if (new_socket < 0) {
+        perror("Accept failed");
+        return 1;
+    }
+
     std::cout << "Client connected!\n";
 
-    // 5. Launch threads
+    // 5. Threads for communication
     std::thread recvThread(receiveMessages, new_socket);
     std::thread sendThread(sendMessages, new_socket);
 
